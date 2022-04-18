ffcd78088c7c57828e8bf041cea0a5484e8ae984
Marial Clarie <marialclarie@gmail.com>
2022-04-18 16:42:55 +0300

    create views and code content

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from .models import Business, Post, Profile, Neighbourhood
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm, UpdateProfileForm, UpdateUserForm, NewNeighbourhoodForm, PostForm, NewBusinessForm
+from django.shortcuts import get_object_or_404, render, redirect
 
 # Create your views here.
